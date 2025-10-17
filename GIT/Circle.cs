cc4a390f792a22767c2f0956b1cbd0d1a2ac7c1f
D1oBrand00 <danikpolzyuk@gmail.com>
2025-10-17 12:56:41 +0300

    Добавление классов Circle и Rectangle

---
+++ GIT/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT
+{
+    public class Circle
+    {
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+}
